227d16254b4ac1c834238d1ba2c5cfdaec912896
dev-abuhasan <ahinstitutte@gmail.com>
2025-01-15 16:39:31 +0600

    clean

--- src/components/templates/laz-load.tsx
+++ src/components/templates/laz-load.tsx
@@ -1,11 +1,3 @@
 'use client';
 import dynamic from 'next/dynamic';
 
-// export const ThemeSwitch = dynamic(() => import('@/components/molecules/theme-switch').then((module) => module.ShopUnderMoneySeeMore), {
-// loading: () => <Loading header />,
-//     ssr: false
-// });
-
-export const LazyThemeSwitch = dynamic(() => import('@/components/molecules/theme-switch'), {
-    ssr: false
-});
